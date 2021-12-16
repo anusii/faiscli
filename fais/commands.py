3aa2b4aaa7888e165fc3832ed5aeeed0306e3702
Graham Williams <Graham.Williams@togaware.com>
2021-12-17 10:22:29 +1100

    Add Grade back in.

--- fais/commands.py
+++ fais/commands.py
@@ -357,7 +357,7 @@ def student(config, uid, session):
         df["Name"] = name
         df["Sex"] = sex
 
-        df = df.iloc[:, [8, 9, 10, 7, 0, 1, 2, 3, 5, 6]]
+        df = df.iloc[:, [8, 9, 10, 7, 0, 1, 2, 3, 4, 5, 6]]
         click.echo(df.to_csv(index=False).strip())
 
     if not config.fake:
